5e0ba108238a3c2da2a466d841a43f47bc0ce437
JasonBuss <jason.buss@customerfx.com>
2019-09-23 13:38:21 -0500

    Added backup DB script

---
+++ BackupDatabases.sql
@@ -0,0 +1,15 @@
+--01:41
+
+BACKUP DATABASE [salesLogix753_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\salesLogix753_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'salesLogix753_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
+GO
+BACKUP DATABASE [saleslogix754_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\saleslogix754_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'saleslogix754_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
+GO
+BACKUP DATABASE [salesLogix80_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\salesLogix80_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'salesLogix80_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
+GO
+BACKUP DATABASE [saleslogix81_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\saleslogix81_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'saleslogix81_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
+GO
+BACKUP DATABASE [saleslogix82_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\saleslogix82_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'saleslogix82_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
+GO
+BACKUP DATABASE [saleslogix83_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\saleslogix83_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'saleslogix83_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
+GO
+BACKUP DATABASE [saleslogix84_eval] TO  DISK = N'C:\Users\jtbAdmin\projects\ICRMDB\backups\saleslogix84_eval_backup.bak' WITH NOFORMAT, NOINIT,  NAME = N'saleslogix84_eval_backup', SKIP, REWIND, NOUNLOAD,  STATS = 10
